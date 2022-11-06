aefe120114e3fed0b20db2f4533d807af8e04910
Yu Liao <yuliao0214@gmail.com>
2022-11-06 17:13:01 +0800

    add debug option

--- aarch64/start-qemu.sh
+++ aarch64/start-qemu.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 #root
 
-qemu-system-aarch64 -M virt -cpu cortex-a53 -nographic -smp 8 -kernel Image \
+if [ $# -eq 1 ] && [ $1 = "debug" ]
+then
+    debug="-S -s"
+fi
+
+qemu-system-aarch64 -M virt -cpu cortex-a53 -nographic -smp 4 -m 2G -kernel Image \
     -append "rootwait root=/dev/vda console=ttyAMA0" \
     -netdev user,id=eth0,hostfwd=tcp::17777-:22 -device virtio-net-device,netdev=eth0 \
     -drive file=rootfs.ext4,if=none,format=raw,id=hd0 -device virtio-blk-device,drive=hd0 \
-    -fsdev local,id=test_dev,path=shared,security_model=none -device virtio-9p-pci,fsdev=test_dev,mount_tag=test_mount
+    -fsdev local,id=test_dev,path=shared,security_model=none -device virtio-9p-pci,fsdev=test_dev,mount_tag=test_mount $debug
 
